2fe662896ffd63228d588113115d6da5c4af1718
Carlos Pigurina <carlinpigu4@gmail.com>
2025-05-29 00:42:27 -0300

    Edited all files and finished darkmode roadmap bonus

--- 03_Personal-Portfolio/js/script.js
+++ 03_Personal-Portfolio/js/script.js
@@ -1,42 +1,22 @@
 // DarK Theme
-const themeButton = document.getElementById("theme-button");
-const body = document.querySelector("body");
-const header = document.querySelector("header");
-const moonIcon = document.querySelector(".fa-moon");
-const sunIcon = document.querySelector(".fa-sun");
-const projectItems = document.querySelectorAll(".project");
-const titleSection = document.querySelector(".title");
+let darkmode = localStorage.getItem("darkmode");
+const darkThemeButton = document.getElementById("theme-button");
+const titleBackgroundColor = document.querySelector(".title");
 
+const enableDarkmode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+}
 
+const disableDarkmode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+    darkmode = "null";
+}
 
-themeButton.addEventListener("click", () => {
-    body.style.transition = "all 0.5s ease-in-out";
-    body.style.backgroundColor = "#121212";
-    body.style.color = "#E0E0E0";
+if (darkmode === "active") enableDarkmode();
 
-    header.style.backgroundColor = "#121212";
-    header.style.color = "#E0E0E0";
-    header.style.transition = "all 0.5s ease-in-out";
-
-    titleSection.style.backgroundColor = "#444444";
-
-    moonIcon.style.display = "none";
-    sunIcon.style.display = "block";
-    sunIcon.style.color = "#FFE87C";
-
-    const navLinks = document.querySelectorAll(".navbar-link");
-    navLinks.forEach((link) => {
-        link.style.color = "#E0E0E0";
-        link.style.transition = "all 0.5s ease-in-out";
-    });
-
-    projectItems.forEach((project) => {
-        project.style.transition = "all 0.5s ease-in-out";
-        project.style.backgroundColor = "#121212";
-        project.style.color = "#E0E0E0";
-    });
-    
-
-    
-
-})
+darkThemeButton.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
