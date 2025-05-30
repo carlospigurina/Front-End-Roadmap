6b5651e6f322efc3a943b546c953a2a5cb65581d
Carlos Pigurina <carlinpigu4@gmail.com>
2025-05-30 17:41:45 -0300

    Completing Changelog project, including dark mode

---
+++ 04_Changelog-Component/script.js
@@ -0,0 +1,21 @@
+// DarK Theme
+let darkmode = localStorage.getItem("darkmode");
+const darkThemeButton = document.getElementById("theme-button");
+
+const enableDarkmode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+    darkmode = "null";
+}
+
+if (darkmode === "active") enableDarkmode();
+
+darkThemeButton.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
